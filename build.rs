b8cae1466e06d75e10e7ab6800c6c408abbef8e1
frk <hazefrk+dev@gmail.com>
2018-01-23 17:59:23 +0100

    🔨 Display semver version in exe info

--- build.rs
+++ build.rs
@@ -1,12 +1,23 @@
 #[cfg(windows)]
 extern crate winres;
 
+use std::process::Command;
+
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("app.ico");
-    res.set_language(0x0409);
+    res.set_icon("app.ico")
+        .set_language(0x0409)
+        .set("FileDescription", &format!("HazeDumper {}", git_semver()));
     res.compile().unwrap();
+}
+
+fn git_semver() -> String {
+    let output = Command::new("git")
+        .args(&["describe", "HEAD"])
+        .output()
+        .unwrap();
+    String::from_utf8(output.stdout).unwrap()
 }
 
 #[cfg(unix)]
